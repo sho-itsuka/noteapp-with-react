c1db69fc76173d9c3a8dc10a4d3575bdd773896d
sho-itsuka <sho.itsuka122@gmail.com>
2023-02-12 14:27:46 +0900

    新しいノートを追加する関数の実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
+
 import Sidebar from './components/Sidebar'
 import Main    from './components/Main'
 
 import './App.css'
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id:      1,
+      title:   "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes)
+  }
+
   return (
     <div className='App'>
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} />
       <Main />
     </div>
   )
